153f477ae30cd5e6438eafb1bc160281519cf380
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-10-10 20:50:39 -0500

    liri-node-app

--- keys.js
+++ keys.js
@@ -5,3 +5,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+  api_key: process.env.BANDSINTOWN_API_KEY
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_API_KEY
+};
--- liri.js
+++ liri.js
@@ -1,2 +1,29 @@
+//Set any environment variables with the dotenv package
 js
 require("dotenv").config();
+
+//Import from keys.js file
+let keys = require("./keys.js");
+
+//Access the keys information
+//EXAMPLE #8 on instructions
+//let spotify = new spotify(keys.spotify);
+//let movieKey = keys.omdb.api_key;
+
+
+//#9 Make it so liri.js can take in one of these commands
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
+
+let omdbApi = require('omdb-client');
+ 
+let params = {
+    apiKey: "https://www.omdbapi.com/?t=" + movieTitles + "&plot=short&apikey=trilogy",
+    title: 'Terminator',
+    year: 2012
+}
+omdbApi.get(params, function(err, data) {
+    // process response...
+});
